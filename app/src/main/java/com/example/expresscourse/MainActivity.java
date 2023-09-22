7b28371a50fd7a3cbaa4637b055f49b30217624c
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-09-22 11:29:12 +0900

    new day

--- app/src/main/java/com/example/expresscourse/MainActivity.java
+++ app/src/main/java/com/example/expresscourse/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.expresscourse;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
+    private TextView mTextView;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
